ec1beb128ee799886c774ee987f71b90a5961b3b
Akshay Sharma <sharma.akshay5@gmail.com>
2022-04-05 20:44:55 +0530

    - Managed parseInt for Binary Number too

--- App.js
+++ App.js
@@ -9,17 +9,33 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
-window.customParseInt = function (num, reg = 10) {
+//We are supporting radix from 2 to 10
+window.customParseInt = function (num, radix = 10) {
   if (!isNaN(num)) {
     if (num.includes('.')) {
       num = num.split('.')[0];
     }
+
+    if (num.split('').some(item => item >= radix)) {
+      if (num.split('')[0] >= radix) {
+        // managing negative case if your are passing 31010 to parse where radix = 2
+        return NaN;
+      }
+
+      num.split('').forEach((item, index) => {
+        if (item >= radix) {
+          num = num.substring(0, index);
+          return;
+        }
+      });
+    }
+
     let i = 0;
     for (let k = num.length - 1; k >= 0; k--) {
       const isNegative = num.charCodeAt(k) == 45;
       //As ascii code for - is 45
       let j = isNegative ? 1 : num.charCodeAt(k) - 48; //ascii code of 0 is 48
-      j = j * Math.pow(reg, num.length - (k + 1));
+      j = j * Math.pow(radix, num.length - (k + 1));
       if (isNegative) {
         i *= -1;
       } else {
@@ -34,16 +50,19 @@ window.customParseInt = function (num, reg = 10) {
 };
 
 const App = () => {
-  const num = '-1';
-
-  console.log('original');
-  console.log(parseInt(num));
-  console.log('custom');
-  console.log(customParseInt(num));
+  const num = '-123.45';
+  const rdx = 10;
 
   return (
     <SafeAreaView style={styles.backgroundStyle}>
-      <Text>Hello World</Text>
+      <Text style={styles.textStyle}>Number : {num}</Text>
+      <Text style={styles.textStyle}>Radix : {rdx}</Text>
+      <Text style={styles.textStyle}>
+        customParseInt(Number) : {customParseInt(num, rdx)}
+      </Text>
+      <Text style={styles.textStyle}>
+        parseInt(Number) : {parseInt(num, rdx)}
+      </Text>
     </SafeAreaView>
   );
 };
@@ -51,7 +70,11 @@ const App = () => {
 const styles = StyleSheet.create({
   backgroundStyle: {
     paddingHorizontal: 24,
-    backgroundColor: 'orange',
+    padding: 30,
+  },
+  textStyle: {
+    fontSize: 20,
+    margin: 20,
   },
 });
 
